c15afbaf3d80ee3d35a9aaa08a7866f6d9287d77
donte04 <ramamsess@gmail.com>
2022-12-08 09:58:22 +0100

    first commit

---
+++ part_1.py
@@ -0,0 +1,27 @@
+from fpdf import FPDF
+
+#create FPDF object
+#Layout ('P','L')
+#Unit ('mm', 'cm', 'in')
+#format ('A3', 'A4' (default), 'A5', 'Letter', 'Legal', (100, 150))
+pdf = FPDF('P', 'mm', 'Letter')
+
+#Add a page
+pdf.add_page()
+
+#specify font
+#fonts ('times', 'courier', 'helvetica', 'symbol', 'zpdfingbats')
+#'B' (bold), 'U' (underline), 'I' (italics), '' (regular), combination (i.e., ('BU'))
+pdf.set_font('helvetica', '', 16)
+
+
+#Add text
+#w = width
+#h = height
+#txt = your text
+#ln (0 False; 1 True - move cursor to next line)
+#border (0 False; 1 True - add border around cell)
+pdf.cell(120, 10, 'Hello World!', ln=True)
+pdf.cell(80, 10, 'Good Bye World!')
+
+pdf.output('pdf_1.pdf')
